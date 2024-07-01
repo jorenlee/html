06d3acd03b184f9bf9665156255efe41b6cf2856
Jo Renlee Luna <jo@bluthrive.com>
2024-07-01 13:16:01 +0800

    update

---
+++ _nuxt/BjDr7ovD.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./DlAUqK2U.js";import{o as s,c as n,a as e}from"./hSa4EajZ.js";const o={},c=e("div",{class:"bg-green-900 p-3"},[e("h1",{class:"text-center text-white text-xs"},[e("span",{class:"font-bold"},"Copyright © 2024 "),e("span",null,"La Salle University - Ozamiz")])],-1),a=[c];function r(_,i){return s(),n("div",null,a)}const d=t(o,[["render",r]]);export{d as _};
