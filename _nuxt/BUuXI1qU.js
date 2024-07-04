40b6f020e51833ceb68d9091c3d238adc481d2b9
Jo Renlee Luna <jo@bluthrive.com>
2024-07-04 22:49:41 +0800

    update

--- _nuxt/BUuXI1qU.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{o as n,c as o,a as t}from"./aN3I3joO.js";const s={},c=t("img",{src:"https://raw.githubusercontent.com/jorenlee/lsu-public-images/main/images/images/banners/Banner2023-2.jpg",class:"align-top w-full h-auto lg:object-fill lg:block hidden"},null,-1),a=[c];function r(l,_){return n(),o("div",null,a)}const u=e(s,[["render",r]]);export{u as _};
