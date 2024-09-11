11ef04ca1fd6cd5917df8de828b1cc93d0a95d32
Jo Renlee Luna <jo@bluthrive.com>
2024-09-12 03:21:14 +0800

    update

--- _nuxt/DgJdZWGX.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{o,c,a as n}from"./BB2tl_MO.js";const t={},_=n("div",{class:"align-top w-full lg:object-fill lg:block hidden bg-green-800 h-[150px]"},null,-1),s=[_];function l(r,a){return o(),c("div",null,s)}const f=e(t,[["render",l]]);export{f as _};
