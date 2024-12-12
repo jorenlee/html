3235c3251692e2b26306ab67b04ddb6f40f60efa
Jo Renlee Luna <jo@bluthrive.com>
2024-12-12 13:54:57 +0800

    update

---
+++ _nuxt/BfxAX7i5.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./DlAUqK2U.js";import{c as a,a as n,o as l}from"./bvft0hb-.js";const t={};function o(r,e){return l(),a("div",null,e[0]||(e[0]=[n("img",{src:"https://lsu-media-styles.sgp1.digitaloceanspaces.com/lsu-public-images/banners/Monthly%20Calendar.png",class:"w-full"},null,-1)]))}const m=s(t,[["render",o]]);export{m as default};
