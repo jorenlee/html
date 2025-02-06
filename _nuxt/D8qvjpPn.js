da72a6c0f1869293e79f99f02958de2e1171c765
kykyostia <kykyostia11@gmail.com>
2025-02-06 13:29:43 +0800

    update

---
+++ _nuxt/D8qvjpPn.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DlAUqK2U.js";import{c as t,a as o,o as r}from"./IwTky3Mg.js";const a={};function c(l,s){return r(),t("div",null,s[0]||(s[0]=[o("img",{src:"https://lsu-media-styles.sgp1.digitaloceanspaces.com/lsu-public-images/banners/mikrotik.png",class:"w-full"},null,-1)]))}const m=e(a,[["render",c]]);export{m as default};
