c820e9e8e8b7a8f261de066dde6c6fc0ef6b675b
kykyostia <kykyostia11@gmail.com>
2025-02-06 17:59:40 +0800

    update

---
+++ _nuxt/DdUSRqlO.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DlAUqK2U.js";import{o as n,c as l,a as t}from"./CLA6j4j1.js";const r={};function c(s,e){return n(),l("div",null,e[0]||(e[0]=[t("div",{class:"align-top w-full lg:object-fill lg:block hidden bg-green-800 h-[150px]"},null,-1)]))}const i=o(r,[["render",c]]);export{i as _};
