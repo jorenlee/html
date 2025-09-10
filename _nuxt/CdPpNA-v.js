6289a950029c11ccf5d81da8c2255c1edca7eff9
jorenlee <jorenleeluna24@gmail.com>
2025-09-10 13:34:31 +0800

    update

---
+++ _nuxt/CdPpNA-v.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./DlAUqK2U.js";import{c as a,a as l,o as c}from"./BLHnK4DS.js";const n={};function o(r,e){return c(),a("div",null,e[0]||(e[0]=[l("img",{src:"https://lsu-media-styles.sgp1.digitaloceanspaces.com/lsu-public-images/banners/School%20Calendar.png",class:"w-full"},null,-1)]))}const m=s(n,[["render",o]]);export{m as default};
